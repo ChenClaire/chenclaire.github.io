26b45b10ddb25da1a4a6e75460c510d5364f7702
Claire Chen <xc2282@tc.columbia.edu>
2017-01-10 21:06:36 -0500

    typo

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ $(window).load(function() {
      $('body, html').animate({ scrollTop: $(ID).offset().top }, '700');
  }
 
- $('.intro .fullName').transition({ y: '15px', opacity: 1 }, 1000);
- $('.intro .keywords').transition({ y: '-10px', opacity: 1, delay: 100 }, 900);
- $('.intro .selfIntro').transition({ opacity: 1 }, 700);
+ // $('.intro .fullName').transition({ y: '15px', opacity: 1 }, 1000);
+ $('.intro .keywords').transition({ y: '10px', opacity: 1, delay: 100 }, 900);
+ $('.intro .introduction').transition({ y: '-10px', opacity: 1, delay: 100 }, 700);
 
  $(".navbar-nav li a").click(function(event) {
      $(".navbar-collapse").collapse('hide');
