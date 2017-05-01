27023ef96717a2377b91ca5ec999b0216fe8335f
Claire Chen <xc2282@tc.columbia.edu>
2017-05-01 16:04:04 -0400

    fancybox testing

--- index.js
+++ index.js
@@ -1,45 +1,37 @@
+function goTo(el) {
+    var ID = "#" + el;
+    $('body, html').animate({ scrollTop: $(ID).offset().top }, '700');
+}
+
 $(window).load(function() {
-   $(".loading").fadeOut(500);
+    $(".loading").fadeOut(500);
 });
 
 
+$("[data-fancybox]").fancybox({
+});
 
+$('.intro .keywords').transition({ y: '10px', opacity: 1, delay: 100 }, 900);
+$('.intro .introduction').transition({ y: '-10px', opacity: 1, delay: 100 }, 700);
 
- function goTo(el) {
-     var ID = "#" + el;
-     $('body, html').animate({ scrollTop: $(ID).offset().top }, '700');
- }
+$(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+});
 
- // $('.intro .fullName').transition({ y: '15px', opacity: 1 }, 1000);
- $('.intro .keywords').transition({ y: '10px', opacity: 1, delay: 100 }, 900);
- $('.intro .introduction').transition({ y: '-10px', opacity: 1, delay: 100 }, 700);
+$(".homeKey").click(function() {
+    window.location.href = "index.html";
+});
 
- $(".navbar-nav li a").click(function(event) {
-     $(".navbar-collapse").collapse('hide');
- });
+$(".topKey").click(function() {
+    $('body, html').animate({ scrollTop: 0 }, '700');
+});
 
- $(".homeKey").click(function() {
-     window.location.href = "index.html";
- });
-
- $(".topKey").click(function() {
- 	$('body, html').animate({ scrollTop: 0 }, '700');
- });
-
- $(window).scroll(function() {
- 	 if ($(window).scrollTop() > 800) {
- 	 	$(".topKey").fadeIn();
- 	 } else {
- 	 	$(".topKey").fadeOut();
- 	 }
- });
-
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 800) {
+        $(".topKey").fadeIn();
+    } else {
+        $(".topKey").fadeOut();
+    }
+});
 
 $('.carousel').carousel();
-
-
-
-
-            // $(".icon-drawing").find("path").addClass("line-drawing");
-       
- 
