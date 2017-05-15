6d69c251d7b70898760ac56d33ec6978d4ee0d14
Claire Chen <xc2282@tc.columbia.edu>
2017-05-14 21:59:48 -0400

    big change

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 function goTo(el) {
     var ID = "#" + el;
+    $(".mobile-nav").fadeOut('400');
     $('body, html').animate({ scrollTop: $(ID).offset().top }, '700');
 }
+
+
+$('[data-toggle="tooltip"]').tooltip()
+
+
 
 $(window).load(function() {
     $(".loading").fadeOut(500);
 });
 
-$('.intro .keywords').transition({ y: '10px', opacity: 1, delay: 100 }, 900);
+// $('.intro .keywords').transition({ y: '10px', opacity: 1, delay: 100 }, 900);
 
-$('.intro .introduction').transition({ y: '-10px', opacity: 1, delay: 100 }, 700);
+// $('.intro .introduction').transition({ y: '-10px', opacity: 1, delay: 100 }, 700);
+$(".menu-trigger").click(function(event) {
+    $(".mobile-nav").slideDown('400');
+});
+
+$(".menu-closer").click(function(event) {
+    $(".mobile-nav").fadeOut('400');
+        
+   
+});
 
 $(".navbar-nav li a").click(function(event) {
     $(".navbar-collapse").collapse('hide');
